d3eea7d333f2a0cdf3318b45ce4189fedba6ae89
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-05-19 21:20:21 +0100

    completed exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -40,15 +40,21 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
-  const parts = [part1, part2, part3];
-  const exercises = [exercises1, exercises2, exercises3];
+  const parts = [part1, part2, part3].map((p) => p.name);
+  const exercises = [part1, part2, part3].map((p) => p.exercises);
 
   return (
     <div>
