737363cd5fed8bd776ef55edee6a24b260c1fb41
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-05-20 18:59:12 +0100

    completed exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 
+const Title = (props) => <h2>{props.name}</h2>;
+
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -9,23 +13,42 @@ const App = () => {
   const incrementGood = () => {
     setGood(good + 1);
   };
+
   const incrementNeutral = () => {
     setNeutral(neutral + 1);
   };
+
   const incrementBad = () => {
     setBad(bad + 1);
   };
 
+  const total = () => {
+    return good + neutral + bad;
+  };
+
+  const average = () => {
+    if (total() === 0) return 0;
+    return (good * 1 + neutral * 0 + bad * -1) / total();
+  };
+
+  const positivePercent = () => {
+    if (total() === 0) return 0;
+    return (good / total()) * 100;
+  };
+
   return (
     <div>
-      <h2>give feedback</h2>
-      <button onClick={incrementGood}>good</button>
-      <button onClick={incrementNeutral}>neutral</button>
-      <button onClick={incrementBad}>bad</button>
-      <h2>statistics</h2>
+      <Title name="give feedback" />
+      <Button onClick={incrementGood} text="good" />
+      <Button onClick={incrementNeutral} text="neutral" />
+      <Button onClick={incrementBad} text="bad" />
+      <Title name="statistics" />
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total()}</div>
+      <div>average {average()}</div>
+      <div>positive {positivePercent()} %</div>
     </div>
   );
 };
