a72350b8d28945378494fe7b8e217393f57373ce
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-05-20 18:16:55 +0100

    completed exercise 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementGood = () => {
+    setGood(good + 1);
+  };
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const incrementBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={incrementGood}>good</button>
+      <button onClick={incrementNeutral}>neutral</button>
+      <button onClick={incrementBad}>bad</button>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  );
+};
+
+export default App;
