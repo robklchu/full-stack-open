59ef6a5706807e0bcf24f2f7f23fea74e4c18ab8
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-05-23 17:16:39 +0100

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
 
+const Persons = ({ group }) => {
+  return (
+    <>
+      {group.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [query, setQuery] = useState("");
+  const [isFilter, setIsFilter] = useState(false);
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+    setIsFilter(true);
+  };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -17,6 +43,10 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault();
+
+    setIsFilter(false);
+    setQuery("");
+
     const existingNames = persons.map((person) => person.name);
     if (existingNames.includes(newName)) {
       alert(`${newName} is already added to phonebook`);
@@ -30,6 +60,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleQueryChange} value={query} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input onChange={handleNameChange} value={newName} />
@@ -42,11 +76,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      {!isFilter && <Persons group={persons} />}
+      {isFilter && <Persons group={filteredPersons} />}
     </div>
   );
 };
