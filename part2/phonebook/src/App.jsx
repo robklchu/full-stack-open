d28ee04f88ee8d2fedc314281880f3624fa8fef5
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-05-26 10:13:03 +0100

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Input = ({ text, handler, value }) => (
   <div>
@@ -41,16 +42,19 @@ const Persons = ({ group }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [query, setQuery] = useState("");
   const [isFilter, setIsFilter] = useState(false);
+
+  useEffect(fetchPersons, []);
+
+  function fetchPersons() {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+  };
 
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(query.toLowerCase())
