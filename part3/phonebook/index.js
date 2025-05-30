7e87ef32618851b2dfa22720722f896cacb83e54
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-05-30 11:08:43 +0100

    exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -48,6 +48,13 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
 });
 
 const PORT = 3001;
