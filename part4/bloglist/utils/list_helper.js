0a5bd93a1920edc48629dad5214e5d6db0b37914
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-06-13 15:13:01 +0100

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+  if (blogs.length === 1) return blogs[0].likes;
+  return blogs.map((blog) => blog.likes).reduce((total, cur) => total + cur);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
