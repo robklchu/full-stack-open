63c7c95655d547ef02ad235403e12aff02d49752
Black Lab <162184251+blacklabp@users.noreply.github.com>
2025-07-07 15:42:13 +0100

    exercise 5.1

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+
+  // useEffect(() => {
+  //   const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+  //   if (loggedUserJSON) {
+  //     const user = JSON.parse(loggedBlogappUser);
+  //     setUser(user);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      // window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+
+      setUser(user);
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    } finally {
+      setUsername("");
+      setPassword("");
+    }
+  }
+
+  function loginForm() {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            name="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            name="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    );
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <p style={{ color: "red" }}>{errorMessage}</p>
+        <h2>log in to application</h2>
+        {loginForm()}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
